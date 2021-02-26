7caa4ae2ac32742e45bb409f20ed8bfe1e63108e
sachinsingh812 <kumarsachinsingh01@gmail.com>
2021-02-26 13:09:14 +0530

    Chat demo

---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,6 @@
+
+const String APP_ID = "4281";
+const String AUTH_KEY = "AOX6sNaqv-x3BQL";
+const String AUTH_SECRET = "NFU95S5WQSpQPBy";
+const String ACCOUNT_ID = "TpuBZox_HPxofh7PVZdP";
+const String DEFAULT_PASS = "xxasBUM3gQs36bhj";
